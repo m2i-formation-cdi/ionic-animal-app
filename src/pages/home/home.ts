536c819e45cd388af7f72b5d075e7c6336d537d4
moi <moi@moi.com>
2018-01-30 10:01:08 +0100

    choix aléatoire d'un animal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -77,4 +77,12 @@ export class HomePage {
 
   }
 
+  /**
+   * Choix aléatoire d'un animal
+   */
+  pickAnimalPosition(){
+    let pos = Math.floor(Math.random() * this.animals.length);
+    return pos;
+  }
+
 }
